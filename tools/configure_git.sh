f5d299f0f44fdbe09e99f6118964bbe488ca5823
Wen Guan <wguan.icedew@gmail.com>
2015-04-27 17:56:59 +0200

    tools

---
+++ tools/configure_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright European Organization for Nuclear Research (CERN)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+
+cp tools/commit-msg .git/hooks/commit-msg
+chmod +x .git/hooks/commit-msg
